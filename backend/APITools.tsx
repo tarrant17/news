d59265cd8cab917041b6602916a1291bfd30da7f
renab <renaudabadie@gmail.com>
2022-03-13 16:57:35 +0100

    flat list scroll infini

---
+++ backend/APITools.tsx
@@ -0,0 +1,18 @@
+import { Article } from "../types/article"
+
+export const transformArticleToLightVersion = (articles: Article[]) => {
+    let articlesLight = []
+    let articleLight
+    articles.forEach(article=>{
+        articleLight = {
+            id: article.id,
+            title: article.title,
+            body: article.body,
+            description: article.description,
+            provider: {name: article.provider.name},
+            image: { url: article.image.url, thumbnail: article.image.thumbnail}
+        }
+        articlesLight.push(articleLight)
+    })
+    return articlesLight
+}
